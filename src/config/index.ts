42ad0337df645b3ef7efa678ab9d054df6c20645
lsy0825 <1262905559@qq.com>
2024-11-12 22:47:12 +0800

    配置环境

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,21 +18,21 @@ const env = (document.documentElement.dataset.env as ENV) || 'stg'
 const config = {
   dev: {
     baseApi: '/admin-api',
-    uploadApi: 'http://140.210.78.20',
+    uploadApi: 'http://140.210.78.20:48080',
     cdn: 'http://xxx.aliyun.com',
     mock: false,
     mockApi: 'https://www.fastmock.site/mock/5841b82d5672783b6fd62bb2a06aeb1f/api'
   },
   stg: {
     baseApi: '/admin-api',
-    uploadApi: 'http://140.210.78.20',
+    uploadApi: 'http://140.210.78.20:48080',
     cdn: 'http://xxx.aliyun.com',
     mock: false,
     mockApi: 'https://www.fastmock.site/mock/5841b82d5672783b6fd62bb2a06aeb1f/api'
   },
   prd: {
     baseApi: '/admin-api',
-    uploadApi: 'http://140.210.78.20',
+    uploadApi: 'http://140.210.78.20:48080',
     cdn: 'http://xxx.aliyun.com',
     mock: false,
     mockApi: 'https://www.fastmock.site/mock/5841b82d5672783b6fd62bb2a06aeb1f/api'
